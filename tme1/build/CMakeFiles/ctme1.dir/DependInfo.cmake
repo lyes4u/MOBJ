31cead897d930b41a6238760e90f4f86d69647de
lyes4u <lyes.bara15@gmail.com>
2021-10-10 18:33:55 +0200

    added intro

---
+++ tme1/build/CMakeFiles/ctme1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/nirvana/MOBJ/src/cBox.c" "/home/nirvana/MOBJ/build/CMakeFiles/ctme1.dir/cBox.c.o"
+  "/home/nirvana/MOBJ/src/cMain.c" "/home/nirvana/MOBJ/build/CMakeFiles/ctme1.dir/cMain.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
